8da3caf0ea0937ed4c801b2a9b51865001a2a17c
Cameron Orosco <cameron.orosco@outlook.com>
2018-05-13 08:00:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-function destructivelyRemoveFirstKitten(name, array) {
-  kittens.shift()
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name, array) {
+  kittens.push("Ralph")
+}
+
+function destructivelyPrependKitten(name, array) {
+  kittens.unshift("Bob")
+}
+
+function destructivelyRemoveLastKitten(name, array) {
+  kittens.pop()
+}
+
+function appendKitten(name, array) {
+  const moreCats = ["...kittens", "Broom"];
+}
+
+function prependKitten(name, array) {
+  const EvenMoreCats = ["Arnold", "...kittens"];
 }
