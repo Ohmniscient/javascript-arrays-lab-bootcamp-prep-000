6f69358c4faec3675a01f2a321f62427e9ded57a
Cameron Orosco <cameron.orosco@outlook.com>
2018-05-13 08:40:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function removeLastKitten() {
-  return kittens.slice(3)
+  return kittens.slice(0, kittens.length - 1);
 }
 
 function removeFirstKitten() {
